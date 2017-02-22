fd3ba65155a7d1c481c6e3fe60b5b9aba1daa0a9
Eduardo <developer.sr@elcultivo.mx>
2017-02-22 14:50:54 -0600

    se uitan las paginas del menu composer

--- app/Http/ViewComposers/ClientMainMenuComposer.php
+++ app/Http/ViewComposers/ClientMainMenuComposer.php
@@ -10,10 +10,6 @@ class ClientMainMenuComposer
 {
 	public function compose(View $view)
 	{
-		$header_items = Page::menuMain()->home(null)->published()->orderBy('order', 'ASC')->get();
-		$footer_items = Page::menuFooter()->home(null)->published()->orderBy('order', 'ASC')->get();
-
-		$view->with('header_items',$header_items);
-		$view->with('footer_items',$footer_items);
+		
 	}
 }
