990942548e5006f8c1f17446b0d7c5e546ce03dc
fectda <fectda@gmail.com>
2017-03-02 19:57:33 -0600

    se crea la variable de ambiente CLTVO_MANUAL_URL

--- resources/views/admin/manuals.blade.php
+++ resources/views/admin/manuals.blade.php
@@ -14,9 +14,14 @@
         'instructions'  =>  'Proximamente'
     ])
 
-    <div class="col-xs-10 col-xs-offset-1 video">
-        <div class="video__wrapper">
-            {{-- <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLWJxD0GLYDMYiHN0BZSZTQZLNfn-rg0ZQ&amp;showinfo=0" frameborder="0" allowfullscreen></iframe> --}}
+    <div class="col s10 offset-s1">
+        <div class="row manuals">
+
+            <div class="manuals__container">
+                <iframe class="manuals__container--video" src="{{ env("CLTVO_MANUAL_URL") }}" frameborder="0" allowfullscreen></iframe>
+            </div>
+
         </div>
     </div>
+
 @endsection
