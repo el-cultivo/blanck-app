13dbf7a33dd8a30ee7fc13492a758dced0a8eb89
Julio Adrián Morales Vásquez <julio@elcultivo.mx>
2017-04-10 15:41:54 -0500

    Se agregan rutas, controlador y opción del admin main menú del Seo Booster

--- database/sets/AssociatePermissionRoleSet.php
+++ database/sets/AssociatePermissionRoleSet.php
@@ -8,81 +8,102 @@ use App\Permission;
 class AssociatePermissionRoleSet extends CltvoSet
 {
     /**
-     * Etiqueta a desplegarse par ainformar final
+     *  Etiqueta a desplegarse para informar al final
      */
-    protected $label =  "Associate permissions and roles";
+    protected $label = 'Associate permissions and roles';
 
     /**
-     * nombre de la clase a ser sembrada
+     *  Nombre de la clase a ser sembrada
      */
-    protected function CltvoGetModelClass(){
-        return "";
+    protected function CltvoGetModelClass()
+    {
+        return '';
     }
 
     /**
-     * valores a ser introducidos en la base
+     *  Valores a ser introducidos en la base
      */
-    protected function CltvoGetItems(){
+    protected function CltvoGetItems()
+    {
         return [
             [
-                "role"          => "super_admin",
-                "permissions"   => []
+                'role'          => 'super_admin',
+                'permissions'   => []
             ],
             [
-                "role"          => "admin",
-                "permissions"   => [
-                    "admin_access",
-                    "system_config",
-                    "manage_users",
-                    "manage_photos",
-                    "associate_photos",
-
-                    "manage_pages_contents",
-
+                'role'          => 'admin',
+                'permissions'   => [
+                    'admin_access',
+                    'system_config',
+                    'manage_users',
+                    'manage_photos',
+                    'associate_photos',
+                    'manage_pages_contents',
+                    'manage_seo_booster'
                 ]
-            ],
+            ]
         ];
     }
 
     /**
-     * metodo de introduccion de valores
-     * @param array   $model_args argumentos que definiran el
-     * @param Command $comand     comando actual
+     * Método de introducción de valores
+     * @param array     $model_args   Argumentos que definiran el sembrado
+     * @param Command   $command      Commando actual
      */
-    protected function CltvoSower(array $model_args, Command $comand){
-        $role = Role::where(["slug"=> $model_args["role"]])->get()->first();
+    protected function CltvoSower(array $model_args, Command $command)
+    {
+        $role = Role::where(['slug' => $model_args['role']])->get()->first();
 
-        if (!$role) {
-            $comand->error($model_args["role"]." role not exist.");
-            return ;
+        if (!$role)
+        {
+            $command->error($model_args['role'] . ' role not exist.');
+            return;
         }
 
-        if( $role->isSuperAdmin() ){
-            foreach (Permission::get() as $permission) {
-                $this->AssociatePermissionAndRole($role,$permission,$comand);
+        if($role->isSuperAdmin())
+        {
+            foreach (Permission::get() as $permission)
+            {
+                $this->AssociatePermissionAndRole($role, $permission, $command);
             }
         }
 
-        foreach ($model_args["permissions"] as $permission_slug) {
-            $permission = Permission::where(["slug"=> $permission_slug])->get()->first();
-            if (!$permission) {
-                $comand->error($permission_slug." permission not exist.");
-                return ;
+        foreach ($model_args['permissions'] as $permission_slug)
+        {
+            $permission = Permission::where(['slug'=> $permission_slug])->get()->first();
+
+            if (!$permission)
+            {
+                $command->error($permission_slug . ' permission not exist.');
+                return;
             }
-            $this->AssociatePermissionAndRole($role,$permission,$comand);
+
+            $this->AssociatePermissionAndRole($role, $permission, $command);
         }
     }
 
-    protected function AssociatePermissionAndRole(Role $role, Permission $permission,Command $comand)
+    /**
+     * Método de introducción de valores
+     * @param Role          $role       Rol para asociar
+     * @param Permission    $permission Permiso a ser asociado
+     * @param Command       $command    Comando actual
+     */
+    protected function AssociatePermissionAndRole(Role $role, Permission $permission, Command $command)
     {
-        if(!$role->permissions()->get()->find($permission)){
-            if ($role->permissions()->save($permission)) {
-                $comand->line('<info>'.$permission->label.':</info>'." successfully associated with ".$role->label.".");
-            }else{
-                $comand->line('<error>'.$permission->label.':</error>'." not successfully associated with ".$role->label.".");
+        if(!$role->permissions()->get()->find($permission))
+        {
+            if ($role->permissions()->save($permission))
+            {
+                $command->line('<info>' . $permission->label . ':</info>' . ' successfully associated with ' . $role->label . '.');
             }
-        }else {
-            $comand->line('<comment>'.$permission->label.':</comment>'." previously associate with ".$role->label.".");
+            else
+            {
+                $command->line('<error>' . $permission->label . ':</error>' . ' not successfully associated with ' . $role->label . '.');
+            }
+        }
+        else
+        {
+            $command->line('<comment>' . $permission->label . ':</comment>' . ' previously associate with ' . $role->label . '.');
         }
     }
 
