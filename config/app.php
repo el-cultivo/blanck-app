e5ea02c1811611a9e4b7dbf8dfb3268a9ba81c65
Eduardo <eduardo@elcultivo.mx>
2017-08-23 15:17:01 -0500

    rorrecion de la variable URL_SITE

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('URL_SITE', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
