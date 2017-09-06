2ef0dff61cedfbeb3d31b820ba622f294235a2c9
Sergio Fonseca <s@elcultivo.mx>
2017-09-06 15:27:50 -0500

    Se modifica titulo para las páginas del admin

--- resources/views/admin/general/head.blade.php
+++ resources/views/admin/general/head.blade.php
@@ -3,7 +3,13 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title') Adminstrador | {{ config( "app.name") }}</title>
+    <title>
+		@if(View::hasSection('title'))
+        	@yield('title') &lsaquo;
+    	@endif
+
+    	Administrador &ndash; {{ env('APP_NAME') }}
+	</title>
 
 	<!-- Fonts -->
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
