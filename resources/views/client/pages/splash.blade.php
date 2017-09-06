39468aa1b6702e573a0f280b1260c27f04156227
Sergio Fonseca <s@elcultivo.mx>
2017-09-06 15:01:54 -0500

    Se corrije titulo del splash

--- resources/views/client/pages/splash.blade.php
+++ resources/views/client/pages/splash.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html class="splash">
     <head>
-        <title>Blank App | El cultivo</title>
+        <title>{{ config('app.name') }} by El cultivo</title>
         <link href="https://fonts.googleapis.com/css?family=Lato:100, 500" rel="stylesheet" type="text/css">
 
         <style>
