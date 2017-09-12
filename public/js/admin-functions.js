fbfda0e39f79c3a99a1b32bfebb2a064ffe377f9
Diego <diego.vid.eco@gmail.com>
2017-09-12 15:12:00 -0500

    limpia las micorrizas

--- public/js/admin-functions.js
+++ public/js/admin-functions.js
@@ -926,32 +926,6 @@ _constants.w.on('load', function () {
 
 console.log('Hola, estás bien sabroso de tu micorriza');
 
-(function ($) {
-
-	$(document).ready(function () {
-
-		// Sidebar Item Display
-		$('.label_JS').click(function () {
-			if (!$(this).hasClass('label_active')) {
-				$('.tree_JS').slideUp();
-				$('.label_JS').removeClass('label_active');
-				$(this).addClass('label_active');
-				$(this).parent().find('.tree_JS').slideDown();
-			}
-		});
-
-		// Sidebar Item Display
-		$('.label_active').parent().find('.tree_JS').slideDown();
-
-		// Trigger Choose File on Media Manager
-		$(document).on('click', '#media-trigger_JS', function () {
-			$('#media-input_JS input').click();
-		}).find('#media-input_JS input').on('click', function (e) {
-			e.stopPropagation();
-		});
-	});
-})(jQuery);
-
 },{"./alerts-controller":3,"./cltvo/constants.js":4,"./functions/dom":6,"./vue/components/cltvo-v-editor":334,"./vue/components/media-manager":338,"./vue/components/multi-images":339,"./vue/components/pages-simple-cruds":340,"./vue/components/single-image":341,"./vue/main-vue":344,"./vue/main/admin":345}],11:[function(require,module,exports){
 (function (global){
 /**
--- resources/assets/js/micorriza-admin.js
+++ resources/assets/js/micorriza-admin.js
@@ -48,33 +48,3 @@ w.on('load', () => {
 
 
 console.log('Hola, estás bien sabroso de tu micorriza');
-
-
-(function($) {
-
-
-	$(document).ready( function() {
-
-		// Sidebar Item Display
-		$('.label_JS').click( function () {
-			if ( !$(this).hasClass('label_active') ) {
-				$('.tree_JS').slideUp();
-				$('.label_JS').removeClass('label_active');
-				$(this).addClass('label_active');
-				$(this).parent().find('.tree_JS').slideDown();
-			}
-		});
-
-		// Sidebar Item Display
-		$('.label_active').parent().find('.tree_JS').slideDown();
-
-		// Trigger Choose File on Media Manager
-		$(document).on('click','#media-trigger_JS', function() {
-			$('#media-input_JS input').click();
-		}).find('#media-input_JS input').on('click', function (e) {
-			e.stopPropagation();
-		});
-
-	});
-
-})(jQuery);
