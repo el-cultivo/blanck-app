ca4774433bb437b62a1f3a8e3be26541ea4f9686
fectda <fectda@gmail.com>
2017-10-14 10:25:20 -0500

    se activa el middleware de language

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 			\App\Http\Middleware\Cltvo\PreviousUrl::class,
-            //\App\Http\Middleware\CltvoLang::class,
+            \App\Http\Middleware\Cltvo\CltvoCurrentLanguage::class,
         ],
 
         'user' => [
