a1b2b8902853b964403bee68b0197598b8c703ba
fectda <fectda@gmail.com>
2017-10-20 10:55:11 -0500

    se traduce el contact form

--- resources/views/client/general/_social.blade.php
+++ resources/views/client/general/_social.blade.php
@@ -1,9 +1,3 @@
-@if ( $social_networks['facebook'] )
-	<a target="_blank" class="{{ $class }} fa fa-facebook-f" href="{{ $social_networks['facebook'] }}"></a>
-@endif
-@if ( $social_networks['twitter'] )
-	<a target="_blank" class="{{ $class }} fa fa-twitter" href="{{ $social_networks['twitter'] }}"></a>
-@endif
-@if ( $social_networks['instagram'] )
-	<a target="_blank" class="{{ $class }} fa fa-instagram" href="{{ $social_networks['instagram'] }}"></a>
-@endif
+@foreach ($social_networks as $social_network => $link )
+	<a target="_blank" class="{{ $class }} fa fa-{{$social_network}}" href="{{ $link }}"></a>
+@endforeach
