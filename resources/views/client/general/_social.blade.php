463c98225fd1ae4fb03dff505b73c51643e37c62
fectda <fectda@gmail.com>
2017-10-20 10:58:51 -0500

    se acorrige el social network

--- resources/views/client/general/_social.blade.php
+++ resources/views/client/general/_social.blade.php
@@ -1,3 +1,5 @@
-@foreach ($social_networks as $social_network => $link )
-	<a target="_blank" class="{{ $class }} fa fa-{{$social_network}}" href="{{ $link }}"></a>
-@endforeach
+@if ($social_networks && is_array($social_networks))
+	@foreach ($social_networks as $social_network => $link )
+		<a target="_blank" class="{{ $class }} fa fa-{{$social_network}}" href="{{ $link }}"></a>
+	@endforeach
+@endif
