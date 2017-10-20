891a6ececd145171b5b29b662003a3d479210b8e
fectda <fectda@gmail.com>
2017-10-20 11:24:35 -0500

    se traducen las vistas del auth

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,19 +4,19 @@
 	<div class="auth page">
 
 		<div class="auth--title">
-			Restablecer Contraseña
+			{!! trans('auth.password_reset.label') !!}
 		</div>
 
 		<form class="auth__form form" role="form" method="POST" action="{{ route('client::pass_reset:post') }}">
 			{{ csrf_field() }}
 
 			<input type="hidden" name="token" value="{{ $token }}" class="auth__form--input form--input">
-			<input id="email" type="email" class="auth__form--input form--input" placeholder="Correo electrónico" name="email" value="{{ $email or old('email') }}" required autofocus>
-			<input id="password" type="password" class="auth__form--input form--input" placeholder="Contraseña" name="password" required>
-			<input id="password-confirm" type="password" class="auth__form--input form--input" placeholder="Confirmar Contraseña" name="password_confirmation" required>
+			<input id="email" type="email" class="auth__form--input form--input" placeholder="{!! trans('auth.password_reset.form.email.placeholder') !!}" name="email" value="{{ $email or old('email') }}" required autofocus>
+			<input id="password" type="password" class="auth__form--input form--input" placeholder="{!! trans('auth.password_reset.form.password.placeholder') !!}" name="password" required>
+			<input id="password-confirm" type="password" class="auth__form--input form--input" placeholder="{!! trans('auth.password_reset.form.password_confirmation.placeholder') !!}" name="password_confirmation" required>
 
 			<button type="submit" class="auth__form--submit form--submit">
-				Restablecer Contraseña
+					{!! trans('auth.password_reset.form.save') !!}
 			</button>
 		</form>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,30 +5,30 @@
 		<div class="auth--wrap wrap">
 
 			<div class="auth--title page-title">
-				Registrar Nuevo Usuario
+				{!! trans('auth.register.label') !!}
 			</div>
 
 			<form class="form" role="form" method="POST" action="{{ route('client::register:post') }}">
 				{{ csrf_field() }}
 				<div class="">
-					<input id="first_name" type="text" class="form--input" placeholder="Nombre" name="first_name" value="{{ old('first_name') }}" required autofocus>
-					<input id="last_name" type="text" class="form--input" placeholder="Apellido" name="last_name" value="{{ old('last_name') }}" required autofocus>
-					<input id="email" type="email" class="form--input" placeholder="Correo Electrónico" name="email" value="{{ old('email') }}" required>
-					<input id="password" type="password" class="form--input" placeholder="Contraseña" name="password" required>
-					<input id="password-confirm" type="password" class="form--input" placeholder="Confirmar contraseña" name="password_confirmation" required>
+					<input id="first_name" type="text" class="form--input" placeholder="{!! trans('auth.register.form.first_name.placeholder') !!}" name="first_name" value="{{ old('first_name') }}" required autofocus>
+					<input id="last_name" type="text" class="form--input" placeholder="{!! trans('auth.register.form.last_name.placeholder') !!}" name="last_name" value="{{ old('last_name') }}" required autofocus>
+					<input id="email" type="email" class="form--input" placeholder="{!! trans('auth.register.form.email.placeholder') !!}" name="email" value="{{ old('email') }}" required>
+					<input id="password" type="password" class="form--input" placeholder="{!! trans('auth.register.form.password.placeholder') !!}" name="password" required>
+					<input id="password-confirm" type="password" class="form--input" placeholder="{!! trans('auth.register.form.password_confirmation.placeholder') !!}" name="password_confirmation" required>
 				</div>
 				<button type="submit" class="form--submit">
-					Crear Cuenta
+					{!! trans('auth.register.form.save') !!}
 				</button>
 			</form>
 
 			<div class="auth__links">
 				<a class="auth__links--link" href="{{ route('client::login:get') }}">
-					Ya estoy registrado
+					{!! trans('auth.register.back_to_login') !!}
 				</a>
 
 				<a class="auth__links--link" href="{{ route('client::pages.index') }}">
-					Ir a la página principal
+					{!! trans('auth.register.back_to_site') !!}
 				</a>
 			</div>
 
